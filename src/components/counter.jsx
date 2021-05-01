a7527b617134f655f89368ce2f7b566a47ad831d
Erick Amezcua <erickamezcua12@gmail.com>
2021-05-01 11:10:40 -0600

    State changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,7 +6,7 @@ class Counter extends Component {
   };
 
   handleIncrement = () => {
-    console.log("Increment click", this);
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
