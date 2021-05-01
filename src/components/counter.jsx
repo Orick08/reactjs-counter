c1dccc8eea93bedff186f15be00b1141a416936b
Erick Amezcua <erickamezcua12@gmail.com>
2021-05-01 13:42:40 -0600

    Handle the counters with parent and reset button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,11 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>
+        <button onClick={() => this.props.onIncrement(this.props.counter)} className='btn btn-secondary btn-sm'>
           Increment
         </button>
         <button onClick={() => this.props.onDelete(this.props.counter.id)} className='btn btn-danger btn-sm m-2'>
@@ -25,12 +17,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
