ee60447594092663867ffa6ef89565d1c47f537b
Erick Amezcua <erickamezcua12@gmail.com>
2021-05-01 11:08:08 -0600

    Clean the code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,22 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
   };
-
-  renderTags() {
-    if (this.state.tags.length === 0) {
-      return <p>There are no tags!</p>;
-    }
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
 
   handleIncrement = () => {
     console.log("Increment click", this);
@@ -31,7 +16,6 @@ class Counter extends Component {
         <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>
           Increment
         </button>
-        {this.renderTags()}
       </div>
     );
   }
