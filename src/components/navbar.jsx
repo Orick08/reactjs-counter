9a20f9dff1435df50b0d06763fe375144faca861
Erick Amezcua <erickamezcua12@gmail.com>
2021-05-01 16:12:29 -0600

    Destructing arguments

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className='navbar navbar-light bg-light'>
-        <div className='container-fluid'>
-          <a className='navbar-brand' href='#'>
-            Navbar <span className='badge badge-pill badge-secondary'>{this.props.totalCounters}</span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+//Stateless functional component
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className='navbar navbar-light bg-light'>
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='#'>
+          Navbar <span className='badge badge-pill badge-secondary'>{totalCounters}</span>
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
