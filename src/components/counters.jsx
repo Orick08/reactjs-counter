8c5ea4769349d67d0db6ede58f1874bb157b4a33
Erick Amezcua <erickamezcua12@gmail.com>
2021-05-01 11:46:37 -0600

    Children component

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -15,7 +15,9 @@ class Counters extends Component {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} selected />
+          <Counter key={counter.id} value={counter.value} selected>
+            <h4>Counter #{counter.id}</h4>
+          </Counter>
         ))}
       </div>
     );
