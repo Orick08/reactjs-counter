cfbbeb85be0760156c940f0a1f1cbc46b80da033
Erick Amezcua <erickamezcua12@gmail.com>
2021-05-01 08:41:18 -0600

    Adds hello world component

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
+import Counter from './components/counter'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Counter/>,
   document.getElementById('root')
 );
 
